5695df8ece125b8f0295cb55918bf5f2fa1ffe21
obaum101 <43905288+omarbaumgartner@users.noreply.github.com>
2021-05-09 22:26:15 +0200

    Fix : Mouvement du tableau de présentation

---
+++ VR Communication/Assets/Scripts/localFreeze.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class localFreeze : MonoBehaviour
+{
+    private float xPosition;
+    private float yPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        xPosition = transform.position.x;
+        yPosition = transform.position.y;
+    }
+
+    // Update is called once per frame
+    protected void LateUpdate()
+    {
+        transform.localEulerAngles = new Vector3(0, 0, 0);
+        transform.position = new Vector3(xPosition, yPosition, transform.position.z);
+        if (transform.position.z > 21.76f) {
+            transform.position = new Vector3(xPosition, yPosition, 21.76f);
+        }
+        if (transform.position.z < 7.51f)
+        {
+            transform.position = new Vector3(xPosition, yPosition, 7.51f);
+        }
+    }
+}
